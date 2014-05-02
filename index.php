e73afef816d849a4ce0423e4f1d8776890898074
Duffed <duffed90@gmail.com>
2014-05-03 00:29:29 +0200

    Init
    Vorarbeit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>		
+		<title>Jaklef - Musiker/Rapper aus Koblenz. YouTube, Facebook, Instagram, Soundcloud</title>	
+		<?php include('include/head.html') ?>
+	</head>
+<body>
+	<?php include('include/header.html') ?>
+	<!-- content -->
+	<h2>Hallo</h2>
+	<p>Hier steht ein text!</p>
+	<!-- content -->
+	<?php include('include/footer.html') ?>
+</body>
+</html>
