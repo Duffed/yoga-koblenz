ae2929224b9fd0a0584bf50e6bb7d4bc52e1b44f
Duffed <duffed90@gmail.com>
2019-07-27 10:32:45 +0200

    Kurse aktualisiert + Rechtschreibfehler

--- kurse.php
+++ kurse.php
@@ -22,55 +22,63 @@
 
 	<blockquote><b>
 		Das, was Du lernen willst zu tun, <br/>
-		lernst Du indem Du es tust.</b>
+		lernst Du, indem Du es tust.</b>
 		<em class="blockquote_author">- Aristoteles</em>
 	</blockquote>
 
 	<h3>Termine</h3>
 	<table id="termine">
 		<tr>
+			<td>Montags:</td>
+			<td>18:00 Uhr</td>
+			<td>Pro Familia</td>
+		</tr>
+		<tr>
 			<td>Dienstags:</td>
-			<td>9:00 Uhr</td>
+			<td>09:00 Uhr <br>
+				18:00 Uhr</td>
+			<td>Pro Familia<br>
+				Gymnatikraum</td>
 		</tr>
 		<tr>
 			<td>Mittwochs:</td>
-			<td>18:00 Uhr und 20:00 Uhr</td>
+			<td>18:00 Uhr <br>
+				20:00 Uhr</td>
+			<td>Pro Familia<br>
+				Pro Familia</td>
 		</tr>
 		<tr>
 			<td>Donnerstags:</td>
-			<td>8:00 Uhr (60 Minuten)<br/>
-			9:15 Uhr <br/>
-			18:00 und 20:00 Uhr</td>
-		</tr>
-		<tr>
-			<td><b>NEU</b> Dienstags:</td>
-			<td>18:00 Uhr <br/>
-			(ab dem 05.09.2017)<br/></td>
+			<td>08:00 Uhr<br>
+				09:15 Uhr<<br>
+				18:00 Uhr<br>
+				20:00 Uhr</td>
+			<td>aktiviko<br>
+				aktiviko<br>
+				Gymnatikraum<br>
+				Gymnatikraum</td>
 		</tr>
 	</table>
 
-	<h3>Kursort</h3>
-	<p>Gruppenraum der Pro Familia <br>
+	<h3>Kursorte</h3>
+	<p><b>Gruppenraum der Pro Familia</b><br>
 	Schenkendorfstr. 24 <br>
 	56068 Koblenz</p>
 
-	<p><b>Donnerstags</b><br>
-	Yogaschule R. Itschert<br>
-	Simrockstr. 7a<br>
+	<p><b>Gymnastikraum des Rot-Weiß- Koblenz Vereins</b><br>
+	Parkstraße 7<br>
 	56075 Koblenz Oberwerth</p>
 
-	<p>
-		<b>Dienstags 18:00 Uhr</b><br>
-		Parkstr. 7 <br>
-		56075 Koblenz Oberwerth
-	</p>
+	<p><b>aktiviko</b><br>
+	St.Josef-Platz 14<br>
+	56068 Koblenz</p>
 
 	<h3>Kursgebühr</h3>
 	<p>12 &euro; pro 90 Minuten <br>
 	10 &euro; pro 60 Minuten (Fr&uuml;hkurs)</p>
 	<p>Die gesamte Gebühr richtet sich nach der Länge des Kurses<br> und sollte bei Beginn des Kurses bezahlt werden.</p>
 
-	<p class="update">Zuletzt aktualisiert: Mai 2017</p>
+	<p class="update">Zuletzt aktualisiert: Juli 2019</p>
 
 	<!-- content -->
 	<?php include('include/footer.html') ?>
