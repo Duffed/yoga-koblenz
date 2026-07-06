cfa75906d001f55b1aaff4c1616c5e67036fd3c1
Duffed <duffed90@gmail.com>
2026-07-06 13:32:44 +0200

    Revert "Convert sitemap and internal links to extensionless URLs"
    This reverts commit cc5ed2bbc591d9a42c8da02f7efe75c3054beca9.

--- mein_yoga.php
+++ mein_yoga.php
@@ -18,7 +18,39 @@
 	<p>In über 30 Jahren Leitung meiner Kurse habe ich erfahren, dass eine liebevolle, annehmende
 	Haltung der Übenden gegenüber ihrer momentanen Lebenssituation eine wesentliche Basis
 	für die Praxis ist. Die jeweiligen Einschränkungen, Schwächen und Fähigkeiten der Übenden
-	werden zum Ausgangspunkt einer individuellen Übungspraxis.</p>
+	werden mit einbezogen. Um dies zu erkennen, gibt es genügend Zeit für Selbstwahrnehmung
+	und Anleitung, das Körperbewusstsein zu schulen. <br>
+	Der Schwerpunkt meiner Übungsstunden liegt in spürender Achtsamkeit und der Förderung
+	von Entspannung. Die Atmung spielt dabei eine zentrale Rolle, denn sie verbindet Körper und
+	Geist.<br>
+	(Britta Hölzel, Hirnforscherin, bezeichnet Achtsamkeit als die beste Burnout-Prophylaxe.)<br>
+	Ich vermittle &Uuml;bungen und eine innere Haltung, die Selbstvertrauen f&ouml;rdern und die Linderung von Stress bewirken.</p>
+
+	<p>Der Atem und die Bewegungsabläufe werden koordiniert und in achtsamer Wiederholung verlangsamt
+	und verfeinert. <br>
+	Ich leite, den anatomisch funktionellen Gesetzmäßigkeiten folgend, Bewegungen präzise an
+	und korrigiere entsprechend. Dabei geht es um ein gezieltes Kräftigen und Dehnen bestimmter
+	Körperregionen und um das Auflösen von Blockaden, um ins Gleichgewicht der Kräfte zurückzufinden. 
+	Entscheidend ist, die individuelle Balance zwischen Bemühen und Loslassen zu finden.</p>
+
+	<p>Jede Übungseinheit ist im Wechsel zwischen Dynamik und Ruhe in sich ausgewogen;
+	Körper, Atem und Geist werden als eine aufeinander bezogene Einheit angesprochen
+	(Asana, Pranayama und Meditation).</p>
+
+	<blockquote>
+		"Alles ist in Bewegung. Alles kommt und geht.<br>
+		Da ist nichts, was wir festhalten können.<br>
+		Beständig ist nur das Fließen.<br>
+		Die Permanenz liegt im Fließen, nicht in der Statik.<br>
+		Achtsamkeit weckt in uns die Fähigkeit, dieses Fließen<br>
+		und zugleich die Ruhe in aller Bewegung zu erfahren."
+		<br>
+		<em class="blockquote_author">- <a href="http://www.west-oestliche-weisheit.de/willigis-jaeger.html">Willigis Jäger </a></em>
+	</blockquote>
+
+	<p>Diese &Uuml;bungsmethode gibt dem menschlichen System einen klugen Impuls zur Selbstregulation.<br>
+	Mein Ziel ist die schrittweise positive Veränderung zum Gesundsein auf allen Ebenen,<br>
+	<b>„aus der Enge in die Weite“</b>.</p>
 	<!-- content -->
 	<?php include('include/footer.html') ?>
 </body>
--- yoga.php
+++ yoga.php
@@ -21,8 +21,101 @@
 	<p>Yoga ist ein Übungsweg, der über mehrere Jahrtausende hinweg in Indien entwickelt und überliefert
 	wurde. Der Sanskritbegriff "Yoga" leitet sich von "Joch" ab, was anschirren, vereinigen
 	bedeutet. Die spirituelle Zielsetzung im Yoga ist die Vereinigung mit dem "höchsten Selbst", ist
-	also ein Weg der Selbsterkenntnis und der inneren Sammlung.</p>
+	die Erfahrung des tiefen Kerns unseres Bewusstseins.</p>
 
+	<h2>Geschichte des Yoga</h2>
+	<p>
+	Die älteste Quelle des Yoga sind die Veden/Upanishaden, die aus dem 6. Jhd. vor Christus stammen.<br>
+	Die Grundlehrschrift des Yoga ist das <a class="yogasutra_link" href="#yogasutra">Yogasutra</a> von Patanjali, die vor etwa 2000 Jahren
+	verfasst wurde. Es ist die Lehrschrift vom menschlichen Geist und seinen Transformationsmöglichkeiten,
+	zeitlos, traditionsübergreifend und universell anwendbar. Hier finden wir Empfehlungen,
+	wie wir durch unser Verhalten zu innerem Frieden finden und ein harmonisches soziales
+	Leben führen können. <br>
+	Das <a class="yogasutra_link" href="#yogasutra">Yogasutra</a> basiert auf Erfahrung und gibt uns zahlreiche mentale Übungen, die uns in
+	Richtung essentielle Freiheit führen möchten.</p>
+
+	<p>Erst im 14. Jahrhundert ist die Hatha-Yoga-Pradipika geschrieben worden. Hier sind zahlreiche
+	Körperhaltungen, auch mit ihren heilenden Kräften, beschrieben. Mit diesem Quellentext wurden
+	erstmals Körperübungen in den Schwerpunkt der Yogapraxis gesetzt.</p>
+
+	<h2>Modernes Yoga</h2>
+	<p>T. Krishnamacharya , einer der bedeutendsten Yogameister des 20. Jahrhundert, war Pionier
+	in Bezug auf die Wiederbelebung der alten Yogalehren. Er passte das Wissen des Yoga an
+	die veränderten Lebensumstände und Bedürfnisse des modernen Menschen an. Er lehrte die
+	großen Yogameister von heute, wie seinen Sohn T.K.V. Desikachar, BKS Iyengar, Sri Pattabhi Jois
+	und viele mehr.</p>
+	<p>Die heutigen unterschiedlichen Yogastile gehen auf die verschiedenen Lehrer zurück, wie zum Beispiel
+	"Iyengar- Yoga" auf BKS Iyengar, hinter dem kraftvollen "Ashtanga-Yoga" steht Sri Patthabhi Jois, "Viniyoga"
+	wird häufig für die Arbeit von T.K.V. Desikachar benutzt.</p>
+	<p>Hatha-Yoga ist ein &uuml;bergeordneter Begriff, mit dem alle Yogarichtungen bezeichnet werden, die Körperarbeit
+	integrieren.</p>
+	<p>Yoga ist ein wunderbar ganzheitlicher Übungsweg. <br>
+	Ich arbeite in <a href="mein_yoga.php">meinem Yoga Stil</a> im Geiste des Yoga-Sutras. So wie Desikachar betont, sollte die
+	Art und Weise des Übens uns "näher zu uns selbst führen".</p>
+
+	<h2 id="yogasutra">Yogasutra</h2>	
+		
+		<p class="topmargin">Patanjali, der bedeutendste Lehrer des Yoga, verfasste um Christi Geburt das Yogasutra.<br></p>
+		<a class="yogasutra_link yogasutra_hide info-text pointer">weiterlesen</a>
+		<!-- Container (toggle) -->
+		<div class="yogasutra_container">
+			<p class="bottommargin">Sutra bedeutet Faden und ist eine Dichtungsform des Sanskrit, der alten Sprache Indiens.
+			Es ist mit nur 195 Sätzen sehr kurz, jeder Einzelne sehr gehaltvoll.
+			Die Sätze gleichen kostbaren Perlen, die durch einen Faden, dem Sutra, wie zu einer Perlenkette zusammengefügt und dadurch zu einem
+			wertvollen Ganzen werden.</p>
+
+			<p>In den vergangenen beiden Jahrtausenden gab es viele Kommentare zum Yogasutra.
+			R.Sriram hat 2002 das Yogasutra im Kontext der Moderne klar übersetzt und erläutert.
+			"In vier Kapiteln geht es darum, das meinende, denkende Selbst zu erforschen und zu verstehen.
+			Das Yogastura inspiriert uns, ihm auf die Schliche zu kommen und einen klaren Weg aus
+			Schwierigkeiten zu finden. Es bietet viele Lösungsmodelle für unterschiedliche Fragen, die sich
+			auch in unserem modernen Leben stellen."</p>
+
+			<p>Im zweiten der insgesamt vier Kapitel gibt Patanjali Anleitung zu Körperübungen: </p>
+			<p><b class="green">a)</b> kraftvoll üben und dabei leicht und unverkrampft bleiben <br>
+			<b class="green">b)</b> die Aufmerksamkeit des Geistes nicht verändern, sondern auf etwas Konstantes ausrichten.</p>
+
+			<p>und beschreibt den bekannten achtgliedrigen Pfad des Yoga:</p>
+
+			<table>
+				<tr>
+					<th>1. Yamas</th>
+					<td>Verhaltensweisen, die ein friedliches Leben im Einklang mit sich selbst und
+						der Welt fördern: <br>
+						Gewaltlosigkeit, Wahrhaftigkeit, Nicht-Stehlen, Mäßigung, Nicht Anhäufen</td>
+				</tr>
+				<tr>
+					<th>2. Niyamas</th>
+					<td>Qualitäten, die unsere persönliche Entwicklung fördern: <br>
+						Reinheit, Zufriedenheit, Disziplin, Selbststudium, Hingabe</td>
+				</tr>
+				<tr>
+					<th>3. Asana</th>
+					<td>eine stabile, angenehme Sitzhaltung</td>
+				</tr>
+				<tr>
+					<th>4. Pranayama</th>
+					<td>Atemführung</td>
+				</tr>
+				<tr>
+					<th>5. Pratyahara</th>
+					<td>das Zurückziehen der Sinne</td>
+				</tr>
+				<tr>
+					<th>6. Dharana</th>
+					<td>Konzentration</td>
+				</tr>
+				<tr>
+					<th>7. Dhyana</th>
+					<td>Meditation</td>
+				</tr>
+				<tr>
+					<th>8. Samadhi</th>
+					<td>Glückseligkeit, vollkommene Erkenntnis, Einheit</td>
+				</tr>
+			</table>
+
+		</div>
 	<!-- content -->
 	<?php include('include/footer.html') ?>
 </body>
